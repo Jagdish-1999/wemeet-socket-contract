bbdb0c49ea7a570a962688063b8fd1c6ce7de11f
Jagdish Chandra <jagdishchandra1368@gmail.com>
2025-07-26 15:04:46 +0530

    make cb as option dependencies

--- src/utils/callbacks.ts
+++ src/utils/callbacks.ts
@@ -2,5 +2,5 @@ import { TypedServerSocket } from "../common/socket.types";
 import type Response from "./response";
 
 
-export type ClientToServerCallback<P, R> = (payload: P, cb: (res: Response<R>) => void, socket?: TypedServerSocket) => void;
-export type ServerToClientCallback<P, R> = (payload: Response<P>, cb: (res: R) => void) => void;
+export type ClientToServerCallback<P, R> = (payload: P, cb?: (res: Response<R>) => void, socket?: TypedServerSocket) => void;
+export type ServerToClientCallback<P, R> = (payload: Response<P>, cb?: (res: R) => void) => void;
