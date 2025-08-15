113dbe2be1e0cac7626f584489cec56449d43685
Jagdish Chandra <jagdishchandra1368@gmail.com>
2025-08-15 07:19:01 +0530

    add io parameter in Clientto server callback.

--- src/utils/callbacks.ts
+++ src/utils/callbacks.ts
@@ -1,6 +1,8 @@
+import { Server } from "socket.io";
 import { TypedServerSocket } from "../common/socket.types";
+import { ClientToServerEventMap, ServerToClientEventMap } from "../types";
 import type Response from "./response";
 
 
-export type ClientToServerCallback<P, R> = (payload: P, cb?: (res: Response<R>) => void, socket?: TypedServerSocket) => void;
+export type ClientToServerCallback<P, R> = (payload: P, cb?: (res: Response<R>) => void, socket?: TypedServerSocket, io?: Server<ClientToServerEventMap, ServerToClientEventMap>) => void;
 export type ServerToClientCallback<P, R> = (payload: Response<P>, cb?: (res: R) => void) => void;
